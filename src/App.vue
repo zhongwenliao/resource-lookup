b54b1050ba41fec759029a33b9ee0fad5ce421ee
v_zwenliao <v_zwenliao@tencent.com>
2019-06-12 10:43:20 +0800

    提交侧边栏完善

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
+    <!-- GlobalHeader头部公共组件 -->
     <GlobalHeader v-if="!isLoading"></GlobalHeader>
-    <router-view/>
+    <div v-if="!isLoading" class="container" style="left: 0px;">
+      <!-- GlobalSlide侧边公共栏组件 -->
+      <GlobalSlide></GlobalSlide>
+      <router-view/>
+    </div>
+    <template v-else>
+      <router-view/>
+    </template>
   </div>
 </template>
 
 <script>
-import GlobalHeader from './common/GlobalHeader'
+// 导入公共头部组件
+import GlobalHeader from './common/GlobalHeader';
+// 导入公共侧边栏组件
+import GlobalSlide from './common/GlobalSlide';
 
 export default {
   name: 'App',
   components: {
-    GlobalHeader
+    GlobalHeader,
+    GlobalSlide
   },
   data () {
     return {
     }
   },
   computed: {
+    // 判断是否是登录页面
     isLoading () {
       return this.$route.name === 'Login';
     }
