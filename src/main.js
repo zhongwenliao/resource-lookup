55e2a9e1d4e06c2592800df066eeec46e5c29344
chineseLiao <lzw1076175198@gmail.com>
2020-04-16 16:25:07 +0800

    应用使用ementui

--- src/main.js
+++ src/main.js
@@ -4,15 +4,20 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.config.silent = false;
 
+// 使用ElementUI组件
+Vue.use(ElementUI);
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
 });
