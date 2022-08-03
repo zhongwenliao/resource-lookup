4babf620edd4d047df15bec05f82e60775973746
liaozhongwen <liaozhongwen@chszantai.com>
2022-08-03 10:29:42 +0800

    修改提交

--- src/components/Resources/HeatMap.vue
+++ src/components/Resources/HeatMap.vue
@@ -1,10 +1,4 @@
 <template>
-  <!-- <div id="heatmap" style=" width:700px; height: 400px;">
-    <img
-      src="https://antai2021.obs.cn-north-1.myhuaweicloud.com/antai/uploadImg/1635996199896asz4.png"
-      style="width:100%; height: 100%"
-    />
-  </div> -->
   <div>
     <div id="map"></div>
     <div>
@@ -39,7 +33,7 @@ export default {
         this.$nextTick(() => {
           this.onLoad(
             data.map(i => ({
-              count: i+1,
+              count: i + 1,
               lat: i.gpsLat,
               lng: i.gpsLng
             }))
@@ -55,6 +49,7 @@ export default {
 
       // 热力图初始化
       this.heatmapOverlay = new T.HeatmapOverlay({
+        backgroundColor: '',
         // 纬度字段名称。
         latField: 'lat',
         // 经度字段名称。
@@ -64,15 +59,17 @@ export default {
         // 缓冲半径。
         radius: 30,
         // 颜色梯度变化
-        gradient: { 0.25: '#A8B8E2', 0.55: 'rgb(0,255,0)', 0.85: 'yellow', 1.0: 'rgb(255,0,0)' },
-        // 透明度
-        opacity: 0.6,
+        gradient: { 0: 'black', 0.25: 'rgb(0,0,255)', 0.55: 'rgb(0,255,0)', 0.85: 'yellow', 1.0: 'rgb(255,0,0)' },
+        // 整个热图的全局不透明度
+        // opacity: 0.4,
         maxOpacity: 1,
         minOpacity: 0,
-        renderer: 'canvas2d'
+        renderer: 'canvas2d',
+        // 模糊因子越高，渐变越平滑
+        blur: 0.85
       })
       this.map.addOverLay(this.heatmapOverlay)
-      console.log(hotMapData);
+      console.log(hotMapData)
       this.heatmapOverlay.setDataSet({
         data: hotMapData,
         max: 300
@@ -85,38 +82,6 @@ export default {
     closeHeatmap() {
       console.log('关闭热力图')
       this.heatmapOverlay.hide()
-    },
-
-    // 这个写法是直接展示热力图
-    initHotMap() {
-      // 创建一个heatmap实例对象
-      // 这里直接指定热点图渲染的div了.heatmap支持自定义的样式方案,具体可看官网api
-      var heatmapInstance = Heatmap.create({
-        container: document.getElementById('heatmap')
-      })
-      // 构建一些随机数据点,这里替换成你的业务数据
-      var points = []
-      var max = 0
-      var width = 600
-      var height = 400
-      var len = 50
-      while (len--) {
-        var val = Math.floor(Math.random() * 100)
-        max = Math.max(max, val)
-        var point = {
-          x: Math.floor(Math.random() * width),
-          y: Math.floor(Math.random() * height),
-          value: val
-        }
-        points.push(point)
-      }
-      console.log(points)
-      var data = {
-        max: max,
-        data: points
-      }
-      // 因为data是一组数据,所以直接setData
-      heatmapInstance.setData(data)
     }
   }
 }
