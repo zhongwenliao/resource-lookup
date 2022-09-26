71a733ac2f4db811eda9390b80d50f15ec35b73a
liaozhongwen <liaozhongwen@chszantai.com>
2022-09-26 10:18:40 +0800

    修改

--- src/common/utils/request.js
+++ src/common/utils/request.js
@@ -9,7 +9,7 @@ const http = axios.create({
 // 请求拦截
 http.interceptors.request.use(
   config => {
-    localStorage.setItem('token','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NTk5NDk4NjMsInVzZXJuYW1lIjoiYWRtaW4ifQ.76V4HD30St6_qctMIEo9aakeEjTRso_oKOrLvxB6org')
+    localStorage.setItem('token','eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjA3MzMzOTcsInVzZXJuYW1lIjoiYWRtaW4ifQ.MNIM0tJ2JyXpsX-vURLIttTaVUpHTJ37q686BQXXGwc')
     //请求头设置
     config.headers['X-Access-Token'] = localStorage.getItem('token')
     return config
